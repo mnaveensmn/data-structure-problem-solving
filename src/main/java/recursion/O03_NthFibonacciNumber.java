f30ada7b0efe47a4aab3ee4148cf604d47dd6078
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-01-03 09:41:43 +0530

    recursion: Implement algorithm find nth fibonacci number
    Implemented the function to find the nth fibonacci number
    using memoization technique and normal method.

--- src/main/java/recursion/O03_NthFibonacciNumber.java
+++ src/main/java/recursion/O03_NthFibonacciNumber.java
@@ -2,7 +2,51 @@ package recursion;
 
 public class O03_NthFibonacciNumber {
 
-    public int getNthFibonacciNumber(int n) {
-        return n;
+    /**
+     * Finding nth fibonacci number
+     *
+     * @param n
+     * @return
+     */
+    public int fibonacci(int n) {
+        System.out.println(n);
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
+
+    /**
+     * Finding nth fibonacci number using memoization technique
+     *
+     * @param n
+     * @param memo
+     * @return
+     */
+    public int fibonacci(int n, int[] memo) {
+        System.out.println(n);
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else if (memo[n] > 0) {
+            return memo[n];
+        }
+        memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+        return memo[n];
+    }
+
+    /**
+     * Finding nth fibonacci number using memoization technique
+     *
+     * @param n
+     * @return
+     */
+    public int fibWithMemo(int n) {
+        int[] memo = new int[n + 1];
+        return fibonacci(n, memo);
+    }
+
 }
--- src/test/java/recursion/O03_NthFibonacciNumberTest.java
+++ src/test/java/recursion/O03_NthFibonacciNumberTest.java
@@ -2,6 +2,7 @@ package recursion;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class O03_NthFibonacciNumberTest {
@@ -9,7 +10,36 @@ public class O03_NthFibonacciNumberTest {
     @Test
     public void shouldReturnNthFibonacciNumber() {
         O03_NthFibonacciNumber nthFibonacciNumber = new O03_NthFibonacciNumber();
-        int n = nthFibonacciNumber.getNthFibonacciNumber(5);
+        int n = nthFibonacciNumber.fibonacci(5);
         assertTrue(true);
     }
+
+    @Test
+    public void shouldReturn5For5thFibonacciNumber() {
+        O03_NthFibonacciNumber nthFibonacciNumber = new O03_NthFibonacciNumber();
+        int n = nthFibonacciNumber.fibonacci(5);
+        assertEquals(5, n);
+    }
+
+    @Test
+    public void shouldReturn8For6thFibonacciNumber() {
+        O03_NthFibonacciNumber nthFibonacciNumber = new O03_NthFibonacciNumber();
+        int n = nthFibonacciNumber.fibonacci(6);
+        assertEquals(8, n);
+    }
+
+    @Test
+    public void shouldReturn5For5thFibonacciNumberUsingMemoization() {
+        O03_NthFibonacciNumber nthFibonacciNumber = new O03_NthFibonacciNumber();
+        int n = nthFibonacciNumber.fibWithMemo(5);
+        assertEquals(5, n);
+    }
+
+    @Test
+    public void shouldReturn8For6thFibonacciNumberUsingMemoization() {
+        O03_NthFibonacciNumber nthFibonacciNumber = new O03_NthFibonacciNumber();
+        int n = nthFibonacciNumber.fibWithMemo(6);
+        assertEquals(8, n);
+    }
+
 }
