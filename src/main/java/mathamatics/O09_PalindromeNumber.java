5d47697f8f97982066ff00363e4f5fadd5253eff
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-01-23 23:54:47 +0530

    palindrom number implementation

---
+++ src/main/java/mathamatics/O09_PalindromeNumber.java
@@ -0,0 +1,16 @@
+package mathamatics;
+
+public class O09_PalindromeNumber {
+
+    public boolean isPalindrome(int n) {
+        int temp = n;
+        int sum = 0;
+        int power = 0;
+        while (n > 0) {
+            int r = n % 10;
+            sum = sum * 10 + r;
+            n = n / 10;
+        }
+        return sum == temp;
+    }
+}
---
+++ src/test/java/mathamatics/O09_PalindromeNumberTest.java
@@ -0,0 +1,28 @@
+package mathamatics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class O09_PalindromeNumberTest {
+    @Test
+    public void shouldReturnTrueForGivenNumber101() {
+        O09_PalindromeNumber palindromeNumber = new O09_PalindromeNumber();
+        boolean actual = palindromeNumber.isPalindrome(101);
+        assertTrue(actual);
+    }
+
+    @Test
+    public void shouldReturnFalseForGivenNumber102() {
+        O09_PalindromeNumber palindromeNumber = new O09_PalindromeNumber();
+        boolean actual = palindromeNumber.isPalindrome(102);
+        assertFalse(actual);
+    }
+
+    @Test
+    public void shouldReturnTrueForGivenNumber0() {
+        O09_PalindromeNumber palindromeNumber = new O09_PalindromeNumber();
+        boolean actual = palindromeNumber.isPalindrome(0);
+        assertTrue(actual);
+    }
+}
