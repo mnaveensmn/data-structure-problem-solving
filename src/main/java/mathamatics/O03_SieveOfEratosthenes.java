65854120412f2b31cb6f22a26487927f8994bab3
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-01-28 23:58:45 +0530

    sieve of eratosthenes

--- src/main/java/mathamatics/O03_SieveOfEratosthenes.java
+++ src/main/java/mathamatics/O03_SieveOfEratosthenes.java
@@ -2,17 +2,17 @@ package mathamatics;
 
 public class O03_SieveOfEratosthenes {
 
-    //Redid - 1
+    //Redid - 2
     public int[] primeNumbers(int n) {
-        int[] primes = new int[n + 1];
         int finalPrimesCount = n - 1;
+        int[] primes = new int[n + 1];
         for (int i = 2; i <= n; i++) {
             primes[i] = 1;
         }
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if (primes[i] == 1) {
                 for (int j = i * 2; j <= n; j += i) {
-                    if (primes[j]==1) {
+                    if (primes[j] == 1) {
                         primes[j] = 0;
                         finalPrimesCount--;
                     }
