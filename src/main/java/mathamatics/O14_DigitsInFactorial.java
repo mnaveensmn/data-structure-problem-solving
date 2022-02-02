ab0583a14b88ea333c20ebf33eaf77f5931db2d2
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-02-02 23:12:11 +0530

    redid: count digits in factorial

--- src/main/java/mathamatics/O14_DigitsInFactorial.java
+++ src/main/java/mathamatics/O14_DigitsInFactorial.java
@@ -2,17 +2,18 @@ package mathamatics;
 
 public class O14_DigitsInFactorial {
 
+    //Redid - 1
     public int countDigits(int n) {
-        if (n < 0) {
-            return 0;
-        }
-        if (n <= 1) {
-            return 1;
-        }
-        double digits = 0;
-        for (int i = 2; i <= n; i++) {
-            digits += Math.log10(i);
-        }
-        return (int) (Math.floor(digits)) + 1;
+       if (n<0) {
+           return 0;
+       }
+       if (n<=1) {
+           return 1;
+       }
+       double digitCount =0;
+       for (int i=2;i<=n;i++) {
+           digitCount+=Math.log10(i);
+       }
+       return (int) (Math.floor(digitCount))+1;
     }
 }
