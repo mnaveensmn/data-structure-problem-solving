a1b6a1369510b9cdaf2e65c70dedb6d14a69f903
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-02-03 22:02:07 +0530

    mathamatics: sieve of eratosthenes

--- src/main/java/mathamatics/O03_SieveOfEratosthenes.java
+++ src/main/java/mathamatics/O03_SieveOfEratosthenes.java
@@ -1,10 +1,12 @@
 package mathamatics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class O03_SieveOfEratosthenes {
 
-    //Redid - 2
+    //Redid - 3
     public int[] primeNumbers(int n) {
-        int finalPrimesCount = n - 1;
         int[] primes = new int[n + 1];
         for (int i = 2; i <= n; i++) {
             primes[i] = 1;
@@ -12,22 +14,16 @@ public class O03_SieveOfEratosthenes {
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if (primes[i] == 1) {
                 for (int j = i * 2; j <= n; j += i) {
-                    if (primes[j] == 1) {
-                        primes[j] = 0;
-                        finalPrimesCount--;
-                    }
+                    primes[j] = 0;
                 }
             }
         }
-        if (finalPrimesCount < 0)
-            return new int[]{};
-        int[] finalPrimes = new int[finalPrimesCount];
-        int primeIndex = 0;
+        List<Integer> primeNumbers = new ArrayList<>();
         for (int i = 2; i < primes.length; i++) {
             if (primes[i] == 1) {
-                finalPrimes[primeIndex++] = i;
+                primeNumbers.add(i);
             }
         }
-        return finalPrimes;
+        return primeNumbers.stream().mapToInt(i -> i).toArray();
     }
 }
