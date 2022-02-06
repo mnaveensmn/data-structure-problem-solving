caf4dd9f909db06cc2cb6815fab451fcbb098361
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-02-06 21:30:06 +0530

    mathematics: count digits

--- src/main/java/mathamatics/O01_CountDigits.java
+++ src/main/java/mathamatics/O01_CountDigits.java
@@ -2,15 +2,17 @@ package mathamatics;
 
 public class O01_CountDigits {
 
+    //Redid -1
     public int loopMethod(int n) {
         int count = 0;
         while (n != 0) {
-            n = n / 10;
             count++;
+            n = n / 10;
         }
         return count;
     }
 
+    //Redid -1
     public int recursiveMethod(int n) {
         if (n == 0) {
             return 0;
@@ -18,9 +20,9 @@ public class O01_CountDigits {
         return 1 + recursiveMethod(n / 10);
     }
 
-    //Redid = 1
+    //Redid = 2
     public int logMethod(int n) {
-        return (int) Math.floor(Math.log10(n))+1;
+        return (int) Math.floor(Math.log10(n)) + 1;
     }
 
 }
