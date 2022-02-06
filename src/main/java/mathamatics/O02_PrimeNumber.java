73d6408250ff9792de0d2ef582607f9c04d0d224
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-02-06 21:36:16 +0530

    mathematics: prime number

--- src/main/java/mathamatics/O02_PrimeNumber.java
+++ src/main/java/mathamatics/O02_PrimeNumber.java
@@ -15,9 +15,9 @@ public class O02_PrimeNumber {
         }
     }
 
-    //Redid -1
+    //Redid -2
     public boolean isPrimeNumberMoreEfficient(int n) {
-        if (n == 1) {
+        if (n == 1 || n == 0) {
             return false;
         }
         if (n == 2 || n == 3) {
@@ -26,8 +26,8 @@ public class O02_PrimeNumber {
         if (n % 2 == 0 || n % 3 == 0) {
             return false;
         }
-        for (int i = 5; i * i <= n; i += 6) {
-            if (n % i == 0 || n % (i = 2) == 0) {
+        for (int i = 5; i <= Math.sqrt(n); i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
                 return false;
             }
         }
