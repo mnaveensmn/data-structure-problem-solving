d3ef82b81d470506d1bb6953c29c137b9f139a49
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-02-11 21:27:11 +0530

    bitwise: unset a bit at nth position of a number

--- src/main/java/bitwise/O01_BitTactics.java
+++ src/main/java/bitwise/O01_BitTactics.java
@@ -8,4 +8,14 @@ public class O01_BitTactics {
         //Step 2: Perform OR operation on leftShifted and n
         return n | leftShifted;
     }
+
+    public int unSetBit(int n, int position) {
+        //Step 1: Left shift 1 to given position
+        int leftShifted = 1 << position;
+        //Step 2: Flip the bits of left shifted number
+        int flipped = ~leftShifted;
+        //Step 3: Perform AND operation on 'flipped' and 'n'
+        return n & flipped;
+
+    }
 }
--- src/test/java/bitwise/O01_BitTacticsTest.java
+++ src/test/java/bitwise/O01_BitTacticsTest.java
@@ -12,6 +12,16 @@ class O01_BitTacticsTest {
     public void shouldSetBitAtNthPositionInGivenNumber() {
         int actual = bitTactics.setBit(16, 3);
         assertEquals(24,actual);
+        actual = bitTactics.setBit(5, 1);
+        assertEquals(7,actual);
+    }
+
+    @Test
+    public void shouldUnSetBitAtNthPositionInGivenNumber() {
+        int actual = bitTactics.unSetBit(24, 3);
+        assertEquals(16,actual);
+        actual = bitTactics.unSetBit(7, 1);
+        assertEquals(5,actual);
     }
 
 }
