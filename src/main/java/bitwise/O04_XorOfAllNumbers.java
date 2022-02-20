a2189d03379f7f90315c47ee215d0eae3490d783
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-02-20 23:57:50 +0530

    bitwise: redid - xor all

--- src/main/java/bitwise/O04_XorOfAllNumbers.java
+++ src/main/java/bitwise/O04_XorOfAllNumbers.java
@@ -2,14 +2,15 @@ package bitwise;
 
 public class O04_XorOfAllNumbers {
 
+    //Redid - 1
     public int xorAll(int n) {
-        int rem = n % 4;
+        int rem = n%4;
         if (rem == 0) {
             return n;
-        } else if (rem == 1) {
+        } else if (rem==1) {
             return 1;
-        } else if (rem == 2) {
-            return n + 1;
+        } else if (rem==2) {
+            return n+1;
         }
         return 0;
     }
