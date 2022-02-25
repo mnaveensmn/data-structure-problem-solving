d0d7d1dd0cfee5cc4ec4911662a24b0e12bebf41
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-02-25 22:58:19 +0530

    bitwise: first set bit

--- src/main/java/bitwise/O12_FirstSetBit.java
+++ src/main/java/bitwise/O12_FirstSetBit.java
@@ -7,13 +7,14 @@ package bitwise;
  */
 public class O12_FirstSetBit {
 
+    //redid - 1
     public int firstBitPosition(int n) {
         if (n == 0) {
             return 0;
         }
         int position = 1;
         while (n > 0) {
-            if ((n & 1) > 0) {
+            if ((n & 1) != 0) {
                 return position;
             }
             position++;
