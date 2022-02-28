6eff24d90cbbd335a69ac20817c07b8c15169873
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-02-28 22:42:41 +0530

    bitwise: two odd occuring.

--- src/main/java/bitwise/O10_TwoOddOccurring.java
+++ src/main/java/bitwise/O10_TwoOddOccurring.java
@@ -4,16 +4,16 @@ import java.util.Arrays;
 
 public class O10_TwoOddOccurring {
 
-    //redid - 1
+    //redid - 2
     public int[] efficientSolution(int[] a) {
         int[] res = new int[2];
         int xor = 0;
         for (int element : a) {
             xor ^= element;
         }
-        int lastSetBit = xor & (-xor);
+        int rightMostSetBit = xor & (~(xor - 1));
         for (int element : a) {
-            if ((element & lastSetBit) != 0) {
+            if ((element & rightMostSetBit) != 0) {
                 res[0] ^= element;
             } else {
                 res[1] ^= element;
