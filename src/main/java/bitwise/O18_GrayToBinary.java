4727351e1d5bf29c0c8d837f2977656234bb0f36
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-03-04 23:43:55 +0530

    bitwise: redid gray to binary

--- src/main/java/bitwise/O18_GrayToBinary.java
+++ src/main/java/bitwise/O18_GrayToBinary.java
@@ -2,6 +2,7 @@ package bitwise;
 
 public class O18_GrayToBinary {
 
+    //redid - 1
     public int convert(int n) {
         if (n == 0) {
             return 0;
@@ -10,12 +11,12 @@ public class O18_GrayToBinary {
         int binary = 1 << msbPosition;
         int lastXor = 1;
         for (int i = msbPosition - 1; i >= 0; i--) {
-            if (((n >> i) & 1) != 0) {
+            if ((n & (1 << i)) != 0) {
                 lastXor ^= 1;
             } else {
                 lastXor ^= 0;
             }
-            binary |= (lastXor << i);
+            binary |= lastXor << i;
         }
         return binary;
     }
