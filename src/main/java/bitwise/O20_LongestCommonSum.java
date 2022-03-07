c84d0288e84769ba545787e3a4f2ad7cd91abf49
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-03-07 22:42:43 +0530

    bitwise: longest common sum

--- src/main/java/bitwise/O20_LongestCommonSum.java
+++ src/main/java/bitwise/O20_LongestCommonSum.java
@@ -1,5 +1,7 @@
 package bitwise;
 
+import java.util.Arrays;
+
 public class O20_LongestCommonSum {
 
     public int naiveSolution(int[] aArr, int[] bArr) {
@@ -10,10 +12,39 @@ public class O20_LongestCommonSum {
                 aSum += aArr[j];
                 bSum += bArr[j];
                 if (aSum == bSum) {
-                    int len = (j > i) ? j - i + 1 : 0;
+                    int len = (j >= i) ? j - i + 1 : 0;
                     if (len > maxLen) {
                         maxLen = len;
                     }
+                }
+            }
+        }
+        return maxLen;
+    }
+
+    public int auxiliarySpaceSolution(int[] aArr, int[] bArr) {
+        int maxLen = 0;
+        int n = aArr.length;
+        int aSum = 0, bSum = 0;
+        int[] diff = new int[2 * n + 1];
+
+        Arrays.fill(diff, -1);
+
+        for (int i = 0; i < n; i++) {
+            aSum += aArr[i];
+            bSum += bArr[i];
+
+            int currDiff = aSum - bSum;
+            int diffIndex = n + currDiff;
+
+            if (currDiff == 0) {
+                maxLen++;
+            } else if (diff[diffIndex] == -1) {
+                diff[diffIndex] = i;
+            } else {
+                int len = i - diff[diffIndex];
+                if (len > maxLen) {
+                    maxLen = len;
                 }
             }
         }
--- src/test/java/bitwise/O20_LongestCommonSumTest.java
+++ src/test/java/bitwise/O20_LongestCommonSumTest.java
@@ -23,7 +23,28 @@ class O20_LongestCommonSumTest {
         aArr = new int[]{0, 0, 1, 0};
         bArr = new int[]{1, 1, 1, 1};
         actual = longestCommonSum.naiveSolution(aArr, bArr);
+        Assertions.assertEquals(1, actual);
+    }
+
+    @Test
+    public void shouldReturnLongestCommonSumOfGivenArraysUsingAuxiliarySpaceSolution() {
+        O20_LongestCommonSum longestCommonSum = new O20_LongestCommonSum();
+        int[] aArr = new int[]{0, 1, 0, 0, 0, 0};
+        int[] bArr = new int[]{1, 0, 1, 0, 0, 1};
+        int actual = longestCommonSum.auxiliarySpaceSolution(aArr, bArr);
+        Assertions.assertEquals(4, actual);
+        aArr = new int[]{0, 1, 0, 1, 1, 1, 1};
+        bArr = new int[]{1, 1, 1, 1, 1, 0, 1};
+        actual = longestCommonSum.auxiliarySpaceSolution(aArr, bArr);
+        Assertions.assertEquals(6, actual);
+        aArr = new int[]{0, 0, 0};
+        bArr = new int[]{1, 1, 1};
+        actual = longestCommonSum.auxiliarySpaceSolution(aArr, bArr);
         Assertions.assertEquals(0, actual);
+        aArr = new int[]{0, 0, 1, 0};
+        bArr = new int[]{1, 1, 1, 1};
+        actual = longestCommonSum.auxiliarySpaceSolution(aArr, bArr);
+        Assertions.assertEquals(1, actual);
     }
 
 }
