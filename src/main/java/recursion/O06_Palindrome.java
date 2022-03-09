c7e105a070633ff9f8a5908ff4ef8123c1e2c1a4
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-03-10 00:34:29 +0530

    recursion: palindrom or not.

---
+++ src/main/java/recursion/O06_Palindrome.java
@@ -0,0 +1,21 @@
+package recursion;
+
+public class O06_Palindrome {
+
+    public boolean isPalindrome(String str) {
+        return isPalindrome(str.toCharArray(), 0, str.length() - 1);
+    }
+
+    public boolean isPalindrome(char[] str, int start, int end) {
+        if (start == end) {
+            return true;
+        }
+        if (str[start] != str[end]) {
+            return false;
+        }
+        if (start < end) {
+            return isPalindrome(str, start + 1, end - 1);
+        }
+        return true;
+    }
+}
---
+++ src/test/java/recursion/O06_PalindromeTest.java
@@ -0,0 +1,21 @@
+package recursion;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class O06_PalindromeTest {
+
+    @Test
+    public void shouldReturnTrueIfGivenStringIsPalindrome() {
+        O06_Palindrome palindrome = new O06_Palindrome();
+        boolean actual = palindrome.isPalindrome("malayalam");
+        Assertions.assertTrue(actual);
+        actual = palindrome.isPalindrome("aa");
+        Assertions.assertTrue(actual);
+        actual = palindrome.isPalindrome("a");
+        Assertions.assertTrue(actual);
+        actual = palindrome.isPalindrome("abcdba");
+        Assertions.assertFalse(actual);
+
+    }
+}
