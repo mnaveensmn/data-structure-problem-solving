046fda2a2c146ca35b0cd1a80c96d30a122e9f21
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-03-11 23:13:01 +0530

    mathematics: factorial of a number using tail recursion.

--- src/main/java/mathamatics/O07_FactorialOfNumber.java
+++ src/main/java/mathamatics/O07_FactorialOfNumber.java
@@ -11,10 +11,22 @@ public class O07_FactorialOfNumber {
     //    }
     //Redid - 1
     public int factorial(int n) {
-       int res = 1;
-       for(int i=2;i<=n;i++) {
-           res *=i;
-       }
-       return res;
+        int res = 1;
+        for (int i = 2; i <= n; i++) {
+            res *= i;
+        }
+        return res;
     }
+
+    public int tailRecursionFactorial(int n) {
+        return tailRecursionFactorial(n, 1);
+    }
+
+    public int tailRecursionFactorial(int n, int result) {
+        if (n == 0) {
+            return result;
+        }
+        return tailRecursionFactorial(n - 1, n * result);
+    }
+
 }
--- src/test/java/mathamatics/O07_FactorialOfNumberTest.java
+++ src/test/java/mathamatics/O07_FactorialOfNumberTest.java
@@ -2,7 +2,7 @@ package mathamatics;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class O07_FactorialOfNumberTest {
 
@@ -10,14 +10,23 @@ class O07_FactorialOfNumberTest {
     public void shouldReturnAFactorialOfGivenNumber() {
         O07_FactorialOfNumber factorialOfNumber = new O07_FactorialOfNumber();
         int actual = factorialOfNumber.factorial(5);
-        assertEquals(120,actual);
+        assertEquals(120, actual);
     }
 
     @Test
     public void shouldReturnAFactorialOfGivenNumber3() {
         O07_FactorialOfNumber factorialOfNumber = new O07_FactorialOfNumber();
         int actual = factorialOfNumber.factorial(3);
-        assertEquals(6,actual);
+        assertEquals(6, actual);
+    }
+
+    @Test
+    public void shouldReturnAFactorialOfGivenNumberUsingTailRecursion() {
+        O07_FactorialOfNumber factorialOfNumber = new O07_FactorialOfNumber();
+        int actual = factorialOfNumber.tailRecursionFactorial(3);
+        assertEquals(6, actual);
+        actual = factorialOfNumber.tailRecursionFactorial(5);
+        assertEquals(120, actual);
     }
 
 }
