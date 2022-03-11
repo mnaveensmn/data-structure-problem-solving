d778508e228859788c75fd2098a7e5c4fde95e60
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-03-11 23:55:20 +0530

    mathematics

--- src/main/java/mathamatics/O07_FactorialOfNumber.java
+++ src/main/java/mathamatics/O07_FactorialOfNumber.java
@@ -2,13 +2,6 @@ package mathamatics;
 
 public class O07_FactorialOfNumber {
 
-    // Recursive methods for finding factorial is extra over head as it uses the stack memory
-    //    public int factorial(int n) {
-    //        if (n<=1){
-    //       return 1;
-    //        }
-    //        return n * factorial(n-1);
-    //    }
     //Redid - 1
     public int factorial(int n) {
         int res = 1;
