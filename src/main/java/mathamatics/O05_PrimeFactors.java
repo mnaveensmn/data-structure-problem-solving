6eb90c4da5aaa412245ad6753c9911589c4adcf2
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-03-12 21:58:06 +0530

    mathematics: prime factors

--- src/main/java/mathamatics/O05_PrimeFactors.java
+++ src/main/java/mathamatics/O05_PrimeFactors.java
@@ -22,42 +22,41 @@ public class O05_PrimeFactors {
 
     //Redid - 1
     public int[] primeFactorMoreEfficientSolution(int n) {
-        List<Integer> primeFactorList = new ArrayList<>();
+        List<Integer> primeFactors = new ArrayList<>();
         if (n <= 1) {
             return new int[]{};
         }
         if (n % 2 == 0) {
             while (n % 2 == 0) {
+                primeFactors.add(2);
                 n = n / 2;
-                primeFactorList.add(2);
             }
         }
         if (n % 3 == 0) {
             while (n % 3 == 0) {
+                primeFactors.add(3);
                 n = n / 3;
-                primeFactorList.add(3);
             }
         }
         for (int i = 5; i <= Math.sqrt(n); i += 6) {
             if (n % i == 0) {
                 while (n % i == 0) {
                     n = n / i;
-                    primeFactorList.add(i);
+                    primeFactors.add(i);
                 }
             }
             if (n % (i + 2) == 0) {
-                while (n % (i + 2) == 0) {
+                while (n % (i = 2) == 0) {
                     n = n / (i + 2);
-                    primeFactorList.add((i + 2));
+                    primeFactors.add(i + 2);
                 }
             }
         }
 
-        if (n>3) {
-            primeFactorList.add(n);
+        if (n > 3) {
+            primeFactors.add(n);
         }
-
-        return primeFactorList.stream().mapToInt(i -> i).toArray();
+        return primeFactors.stream().mapToInt(i -> i).toArray();
     }
 
     public int[] getPrimeFactor(int n) {
