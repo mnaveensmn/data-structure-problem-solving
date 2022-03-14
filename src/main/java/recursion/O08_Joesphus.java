8ebc1afd3f5f329272cb66b4a50bf1f93a24618a
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-03-14 22:49:31 +0530

    recursion: joesphus

---
+++ src/main/java/recursion/O08_Joesphus.java
@@ -0,0 +1,12 @@
+package recursion;
+
+public class O08_Joesphus {
+
+    public int survivingPosition(int n, int k) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return (survivingPosition(n - 1, k) + (k - 1)) % n + 1;
+        }
+    }
+}
---
+++ src/test/java/recursion/O08_JoesphusTest.java
@@ -0,0 +1,17 @@
+package recursion;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class O08_JoesphusTest {
+
+    @Test
+    public void shouldReturnSurvivingPosition() {
+        O08_Joesphus joesphus = new O08_Joesphus();
+        int actual = joesphus.survivingPosition(5, 2);
+        Assertions.assertEquals(3, actual);
+        actual = joesphus.survivingPosition(14, 2);
+        Assertions.assertEquals(13, actual);
+    }
+
+}
