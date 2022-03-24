71129296d65486141108d4ff72a7b5f1f8bad6a1
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-03-24 22:10:31 +0530

    recursion: Joesphus problem

--- src/main/java/recursion/O08_Joesphus.java
+++ src/main/java/recursion/O08_Joesphus.java
@@ -2,12 +2,11 @@ package recursion;
 
 public class O08_Joesphus {
 
-    //redid - 1
+    //redid - 2
     public int survivingPosition(int n, int k) {
         if (n == 1) {
             return 1;
-        } else {
-            return (survivingPosition(n - 1, k) + k - 1) % n + 1;
         }
+        return (survivingPosition(n - 1, k) + k - 1) % n + 1;
     }
 }
