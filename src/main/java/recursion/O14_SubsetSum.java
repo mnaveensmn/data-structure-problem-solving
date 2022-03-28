cd358f47f2c72a262b6326169ec677566ef01537
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-03-28 08:50:56 +0530

    recursion: subset sum

---
+++ src/main/java/recursion/O14_SubsetSum.java
@@ -0,0 +1,11 @@
+package recursion;
+
+public class O14_SubsetSum {
+
+    public int subsetSum(int[] arr, int n, int sum) {
+        if (n == 0) {
+            return (sum == 0) ? 1 : 0;
+        }
+        return subsetSum(arr, n - 1, sum) + subsetSum(arr, n - 1, sum - arr[n - 1]);
+    }
+}
---
+++ src/test/java/recursion/O14_SubsetSumTest.java
@@ -0,0 +1,20 @@
+package recursion;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class O14_SubsetSumTest {
+
+    @Test
+    public void shouldReturnSubsetSumForGivenSumArray() {
+        O14_SubsetSum subsetSum = new O14_SubsetSum();
+        int actual = subsetSum.subsetSum(new int[]{10,15,25},3,25);
+        Assertions.assertEquals(2,actual);
+        actual = subsetSum.subsetSum(new int[]{5,1,4,2,3},5,5);
+        Assertions.assertEquals(3,actual);
+        actual = subsetSum.subsetSum(new int[]{5,3,3,1},4,6);
+        Assertions.assertEquals(2,actual);
+    }
+}
