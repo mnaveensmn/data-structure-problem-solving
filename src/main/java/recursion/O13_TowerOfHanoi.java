4b3bc4ff07d6fcf8301779f29fd25cc5991b7c70
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-03-31 23:30:41 +0530

    recursion: tower of hanoi

--- src/main/java/recursion/O13_TowerOfHanoi.java
+++ src/main/java/recursion/O13_TowerOfHanoi.java
@@ -2,14 +2,14 @@ package recursion;
 
 public class O13_TowerOfHanoi {
 
-    //redid - 1
+    //redid - 2
     public void TOH(int n, char a, char b, char c) {
         if (n == 1) {
-            System.out.println(a + " to " + c);
+            System.out.println("Move disk " + n + " from " + a + " to " + c);
             return;
         }
-        TOH(n-1, a, c, b);
-        System.out.println(a + " to " + c);
-        TOH(n-1, b, a, c);
+        TOH(n - 1, a, c, b);
+        System.out.println("Move disk " + n + " from " + a + " to " + c);
+        TOH(n - 1, b, a, c);
     }
 }
