b2ed8d93428f41f79e312037e0ab5a09c8565ba1
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-04-05 13:53:21 +0530

    array: insertion and deletion

--- src/main/java/array/O00_Array.java
+++ src/main/java/array/O00_Array.java
@@ -11,4 +11,56 @@ public class O00_Array {
         length = 0;
         this.size = size;
     }
+
+    public void insert(int element) {
+        if (length < size) {
+            array[length] = element;
+            length++;
+        }
+    }
+
+    /***
+     * Insert an element at given position
+     * Here parameter position is zero indexed
+     * @param element
+     * @param position
+     */
+    public void insert(int element, int position) {
+        if (length < size && position < size) {
+            for (int i = length; i >= position; i--) {
+                array[i] = array[i - 1];
+            }
+            array[position] = element;
+            length++;
+        }
+
+    }
+
+    public int search(int element) {
+        for (int i = 0; i < length; i++) {
+            if (array[i] == element) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void delete(int element) {
+        int position = search(element);
+        if (position != -1) {
+            int i;
+            for (i = position + 1; i < length; i++) {
+                array[i - 1] = array[i];
+            }
+            array[i - 1] = 0;
+            length--;
+        }
+    }
+
+    public void traverse() {
+        for (int i = 0; i < length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
 }
--- src/test/java/array/O00_ArrayTest.java
+++ src/test/java/array/O00_ArrayTest.java
@@ -5,23 +5,90 @@ import org.junit.jupiter.api.Test;
 
 class O00_ArrayTest {
 
-    public O00_Array array;
+
 
     public O00_ArrayTest() {
         createArrayForTest();
     }
 
     private void createArrayForTest() {
-        array = new O00_Array(10);
+
     }
 
     @Test
     public void shouldInitiateArrayWithSize() {
+        O00_Array array = array = new O00_Array(10);
         Assertions.assertNotNull(array);
         Assertions.assertEquals(10, array.size);
         Assertions.assertEquals(0, array.length);
         int[] expectedArray = new int[10];
         Assertions.assertArrayEquals(expectedArray, array.array);
+    }
+
+    @Test
+    public void shouldInsertElementAtEndOfTheArray() {
+        O00_Array array = new O00_Array(2);
+        array.insert(1);
+        array.insert(2);
+        int[] expectedArray = new int[2];
+        expectedArray[0]=1;
+        expectedArray[1]=2;
+        Assertions.assertArrayEquals(expectedArray, array.array);
+        array.insert(2);
+        Assertions.assertArrayEquals(expectedArray, array.array);
+    }
+
+    @Test
+    public void shouldInsertElementAtGivenPosition() {
+        O00_Array array = new O00_Array(5);
+        array.insert(1);
+        array.insert(3);
+        array.insert(4);
+        array.insert(5);
+        array.insert(2,1);
+        array.insert(6,5);
+        int[] expectedArray = new int[5];
+        expectedArray[0]=1;
+        expectedArray[1]=2;
+        expectedArray[2]=3;
+        expectedArray[3]=4;
+        expectedArray[4]=5;
+        Assertions.assertArrayEquals(expectedArray, array.array);
+    }
+
+    @Test
+    public void shouldSearchElementInTheArrayAndReturnIndexIfElementFound() {
+        O00_Array array = new O00_Array(5);
+        array.insert(1);
+        array.insert(3);
+        array.insert(4);
+        array.insert(5);
+        array.insert(2,1);
+        int actualIndex = array.search(2);
+        Assertions.assertEquals(1,actualIndex);
+        actualIndex = array.search(5);
+        Assertions.assertEquals(4,actualIndex);
+        actualIndex = array.search(6);
+        Assertions.assertEquals(-1,actualIndex);
+    }
+
+    @Test
+    public void shouldDeleteElementInArrayF() {
+        O00_Array array = new O00_Array(5);
+        array.insert(1);
+        array.insert(3);
+        array.insert(4);
+        array.insert(5);
+        array.insert(2,1);
+        array.delete(3);
+        array.traverse();
+        int[] expectedArray = new int[5];
+        expectedArray[0]=1;
+        expectedArray[1]=2;
+        expectedArray[2]=4;
+        expectedArray[3]=5;
+        Assertions.assertArrayEquals(expectedArray, array.array);
+
     }
 
 
