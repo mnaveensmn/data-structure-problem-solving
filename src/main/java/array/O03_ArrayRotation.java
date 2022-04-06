b568ae3e6ffb369cfc74368f42f242089d0b2231
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-04-06 08:45:09 +0530

    array: array rotation using rotate one at a time method

--- src/main/java/array/O03_ArrayRotation.java
+++ src/main/java/array/O03_ArrayRotation.java
@@ -1,2 +1,41 @@
-package array;public class O03_ArrayRotation {
+package array;
+
+public class O03_ArrayRotation {
+
+    public int[] rotateUsingTempArr(int[] arr, int d) {
+        int n = arr.length;
+        d = d % n;
+        int[] temp = new int[d];
+        for (int i = 0; i < d; i++) {
+            temp[i] = arr[i];
+        }
+        // i - rotatedArrayIndex
+        // j = originalArrayIndex
+        int i = 0;
+        for (int j = d; j < n; j++) {
+            arr[i++] = arr[j];
+        }
+        // j - tempArrayIndex
+        for (int j = 0; j < d; j++) {
+            arr[i++] = temp[j];
+        }
+        return arr;
+    }
+
+    public int[] rotateOneAtTime(int[] arr, int d) {
+        int n = arr.length;
+        d = d % n;
+        for (int i = 0; i < d; i++) {
+            int temp = arr[0];
+            for (int j = 0; j < n - 1; j++) {
+                arr[j] = arr[j + 1];
+            }
+            arr[n - 1] = temp;
+        }
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        return arr;
+    }
 }
--- src/test/java/array/O03_ArrayRotationTest.java
+++ src/test/java/array/O03_ArrayRotationTest.java
@@ -1,4 +1,38 @@
-import static org.junit.jupiter.api.Assertions.*;
+package array;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class O03_ArrayRotationTest {
-  
+
+    @Test
+    public void shouldRotateArrayUsingTempArrayMethod() {
+        O03_ArrayRotation arrayRotation = new O03_ArrayRotation();
+        int[] actualArray = arrayRotation.rotateUsingTempArr(new int[]{1, 2, 3, 4, 5}, 3);
+        Assertions.assertArrayEquals(new int[]{4, 5, 1, 2, 3}, actualArray);
+        actualArray = arrayRotation.rotateUsingTempArr(new int[]{1, 2, 3, 4, 5, 6}, 1);
+        Assertions.assertArrayEquals(new int[]{2, 3, 4, 5, 6, 1}, actualArray);
+        actualArray = arrayRotation.rotateUsingTempArr(new int[]{1, 2, 3, 4, 5, 6}, 6);
+        Assertions.assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6}, actualArray);
+        actualArray = arrayRotation.rotateUsingTempArr(new int[]{1, 2, 3, 4, 5, 6}, 0);
+        Assertions.assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6}, actualArray);
+        actualArray = arrayRotation.rotateUsingTempArr(new int[]{1, 2, 3, 4, 5, 6}, 4);
+        Assertions.assertArrayEquals(new int[]{5, 6, 1, 2, 3, 4}, actualArray);
+    }
+
+    @Test
+    public void shouldRotateArrayUsingOneByOneMethod() {
+        O03_ArrayRotation arrayRotation = new O03_ArrayRotation();
+        int[] actualArray = arrayRotation.rotateOneAtTime(new int[]{1, 2, 3, 4, 5}, 3);
+        Assertions.assertArrayEquals(new int[]{4, 5, 1, 2, 3}, actualArray);
+        actualArray = arrayRotation.rotateOneAtTime(new int[]{1, 2, 3, 4, 5, 6}, 1);
+        Assertions.assertArrayEquals(new int[]{2, 3, 4, 5, 6, 1}, actualArray);
+        actualArray = arrayRotation.rotateOneAtTime(new int[]{1, 2, 3, 4, 5, 6}, 6);
+        Assertions.assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6}, actualArray);
+        actualArray = arrayRotation.rotateOneAtTime(new int[]{1, 2, 3, 4, 5, 6}, 0);
+        Assertions.assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6}, actualArray);
+        actualArray = arrayRotation.rotateOneAtTime(new int[]{1, 2, 3, 4, 5, 6}, 4);
+        Assertions.assertArrayEquals(new int[]{5, 6, 1, 2, 3, 4}, actualArray);
+    }
+
 }
