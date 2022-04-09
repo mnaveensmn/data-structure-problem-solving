bc0c789290d1e61a91d5691ff3191fd3c163540e
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-04-09 23:34:46 +0530

    recursion: permutation of string.

--- src/main/java/recursion/O01_PermutationOfString.java
+++ src/main/java/recursion/O01_PermutationOfString.java
@@ -6,6 +6,7 @@ public class O01_PermutationOfString {
     public void permute(String str, int l, int r) {
         if (l == r) {
             System.out.println(str);
+            return;
         }
         for (int i = l; i <= r; i++) {
             str = swap(str, l, i);
