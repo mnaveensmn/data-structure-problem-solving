e60ba6fdd5f9237e993aba0ba14148d1ea8b9e27
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-04-10 23:10:53 +0530

    array: reverse an array

--- src/main/java/array/O02_ReverseArray.java
+++ src/main/java/array/O02_ReverseArray.java
@@ -2,7 +2,7 @@ package array;
 
 public class O02_ReverseArray {
 
-    //Redid - 1
+    //Redid - 2
     public void reverse(char[] arr, int start, int end) {
         if (start >= end) {
             return;
