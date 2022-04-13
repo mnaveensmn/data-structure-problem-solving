17ad8724f741e065e91494cb695c7d90539d4707
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-04-13 06:45:14 +0530

    array: kadanes algorithm - largest sum subarray.

---
+++ src/test/java/array/O07_KadaneAlgorithmTest.java
@@ -0,0 +1,18 @@
+package array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class O07_KadaneAlgorithmTest {
+
+    @Test
+    public void shouldReturnLargestSumOfSubArray() {
+        O07_KadaneAlgorithm kadaneAlgorithm = new O07_KadaneAlgorithm();
+        int actual = kadaneAlgorithm.largestSumSubArray(new int[]{-3, 4, -1, -2, 1, 5});
+        assertEquals(7, actual);
+        actual = kadaneAlgorithm.largestSumSubArray(new int[]{-1, 1, 5, -4, -2, 3, -2, 6});
+        assertEquals(7, actual);
+    }
+
+}
