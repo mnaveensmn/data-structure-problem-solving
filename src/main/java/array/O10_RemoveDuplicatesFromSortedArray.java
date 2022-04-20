8fb0f28379ecb586808545bd74aec4f927facabd
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-04-20 05:41:43 +0530

    array: remove duplicates from array.

--- src/main/java/array/O10_RemoveDuplicatesFromSortedArray.java
+++ src/main/java/array/O10_RemoveDuplicatesFromSortedArray.java
@@ -2,18 +2,16 @@ package array;
 
 public class O10_RemoveDuplicatesFromSortedArray {
 
-    public int[] removeDuplicates(int[] arr) {
+    public int[] naiveSolution(int[] arr) {
         int[] temp = new int[arr.length];
         temp[0] = arr[0];
         int tempArrLen = 1;
-
         for (int i = 1; i < arr.length; i++) {
             if (temp[tempArrLen - 1] != arr[i]) {
                 temp[tempArrLen] = arr[i];
                 tempArrLen++;
             }
         }
-
         for (int i = 0; i < arr.length; i++) {
             if (i < tempArrLen) {
                 arr[i] = temp[i];
@@ -21,7 +19,17 @@ public class O10_RemoveDuplicatesFromSortedArray {
                 arr[i] = 0;
             }
         }
-
         return arr;
+    }
+
+    public int efficientSolution(int[] arr) {
+        int res = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[res - 1]) {
+                arr[res] = arr[i];
+                res++;
+            }
+        }
+        return res;
     }
 }
--- src/test/java/array/O10_RemoveDuplicatesFromSortedArrayTest.java
+++ src/test/java/array/O10_RemoveDuplicatesFromSortedArrayTest.java
@@ -8,8 +8,17 @@ class O10_RemoveDuplicatesFromSortedArrayTest {
     @Test
     public void shouldRemoveDuplicatesFromSortedArray() {
         O10_RemoveDuplicatesFromSortedArray removeDuplicatesFromSortedArray = new O10_RemoveDuplicatesFromSortedArray();
-        int[] actual = removeDuplicatesFromSortedArray.removeDuplicates(new int[]{1, 1, 1, 2, 3, 3, 4, 4, 4});
+        int[] actual = removeDuplicatesFromSortedArray.naiveSolution(new int[]{1, 1, 1, 2, 3, 3, 4, 4, 4});
         Assertions.assertArrayEquals(new int[]{1, 2, 3, 4, 0, 0, 0, 0, 0}, actual);
+    }
+
+    @Test
+    public void shouldRemoveDuplicatesFromSortedArrayUsingEfficientSolution() {
+        O10_RemoveDuplicatesFromSortedArray removeDuplicatesFromSortedArray = new O10_RemoveDuplicatesFromSortedArray();
+        int actual = removeDuplicatesFromSortedArray.efficientSolution(new int[]{1, 1, 1, 2, 3, 3, 4, 4, 4});
+        Assertions.assertEquals(4, actual);
+        actual = removeDuplicatesFromSortedArray.efficientSolution(new int[]{1, 1});
+        Assertions.assertEquals(1, actual);
     }
 
 }
