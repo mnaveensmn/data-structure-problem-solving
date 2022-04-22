538de6dd5f17b4b21d6e986e69a1f901bbc83b15
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-04-22 22:02:14 +0530

    array: move zeros to end

--- src/main/java/array/O11_MoveZerosEnd.java
+++ src/main/java/array/O11_MoveZerosEnd.java
@@ -2,15 +2,15 @@ package array;
 
 public class O11_MoveZerosEnd {
 
+    //redid - 1
     public void moveZeros(int[] arr) {
         int n = arr.length;
         int lastNonZeroIndex = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] != 0) {
                 int temp = arr[lastNonZeroIndex];
                 arr[lastNonZeroIndex] = arr[i];
                 arr[i] = temp;
-
                 lastNonZeroIndex++;
             }
         }
