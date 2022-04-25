1b96b8cd09af108a733e403e41512d22ccb52a61
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-04-25 13:44:09 +0530

    array: frequency of unsorted array

--- src/main/java/array/O14_FrequencyOfSortedArray.java
+++ src/main/java/array/O14_FrequencyOfSortedArray.java
@@ -18,4 +18,25 @@ public class O14_FrequencyOfSortedArray {
             System.out.println(arr[n - 1] + " ==> " + 1);
         }
     }
+
+    public int[] frequencyOfUnSortedArrayUsingNegativeApproach(int[] arr) {
+        int n = arr.length;
+        int i = 0;
+        while (i < n) {
+            if (arr[i] <= 0) {
+                i++;
+                continue;
+            }
+            int elementIndex = arr[i] - 1;
+            if (arr[elementIndex] > 0) {
+                arr[i] = arr[elementIndex];
+                arr[elementIndex] = -1;
+            } else {
+                arr[elementIndex]--;
+                i++;
+                arr[i] = 0;
+            }
+        }
+        return arr;
+    }
 }
--- src/test/java/array/O14_FrequencyOfSortedArrayTest.java
+++ src/test/java/array/O14_FrequencyOfSortedArrayTest.java
@@ -1,6 +1,9 @@
 package array;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 class O14_FrequencyOfSortedArrayTest {
 
@@ -11,4 +14,16 @@ class O14_FrequencyOfSortedArrayTest {
         frequencyOfSortedArray.frequencyOfSortedArray(new int[]{1, 1, 2, 3});
         frequencyOfSortedArray.frequencyOfSortedArray(new int[]{1});
     }
+
+    @Test
+    public void shouldPrintFrequencyOfUnsortedArrayUsingNegativeMethod() {
+        O14_FrequencyOfSortedArray frequencyOfSortedArray = new O14_FrequencyOfSortedArray();
+        int[] actual = frequencyOfSortedArray.frequencyOfUnSortedArrayUsingNegativeApproach(new int[]{1, 1, 1, 2, 2, 3, 3});
+        System.out.println(Arrays.toString(actual));
+        Assertions.assertArrayEquals(new int[]{-3, -2, -2, 0, 0, 0, 0}, actual);
+        actual = frequencyOfSortedArray.frequencyOfUnSortedArrayUsingNegativeApproach(new int[]{1, 1, 2, 3});
+        Assertions.assertArrayEquals(new int[]{-2, -1, -1, 0}, actual);
+        frequencyOfSortedArray.frequencyOfUnSortedArrayUsingNegativeApproach(new int[]{1});
+    }
+
 }
