c97564a9b2ae50eb5d0b255db5d704593afa1793
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-04-26 16:13:40 +0530

    array: frequency of element in the array

--- src/main/java/array/O14_FrequencyOfArray.java
+++ src/main/java/array/O14_FrequencyOfArray.java
@@ -1,6 +1,6 @@
 package array;
 
-public class O14_FrequencyOfSortedArray {
+public class O14_FrequencyOfArray {
 
     public void frequencyOfSortedArray(int[] arr) {
         int n = arr.length;
@@ -36,6 +36,20 @@ public class O14_FrequencyOfSortedArray {
                 arr[i] = 0;
                 i++;
             }
+        }
+        return arr;
+    }
+
+    public int[] frequencyOfUnSortedArrayUsingAddingNApproach(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            arr[i] = arr[i] - 1;
+        }
+        for (int i = 0; i < n; i++) {
+            arr[arr[i] % n] = arr[arr[i] % n] + n;
+        }
+        for (int i = 0; i < n; i++) {
+            arr[i] = arr[i] / n;
         }
         return arr;
     }
--- src/test/java/array/O14_FrequencyOfSortedArrayTest.java
+++ src/test/java/array/O14_FrequencyOfSortedArrayTest.java
@@ -7,7 +7,7 @@ class O14_FrequencyOfSortedArrayTest {
 
     @Test
     public void shouldPrintFrequencyOfSortedArray() {
-        O14_FrequencyOfSortedArray frequencyOfSortedArray = new O14_FrequencyOfSortedArray();
+        O14_FrequencyOfArray frequencyOfSortedArray = new O14_FrequencyOfArray();
         frequencyOfSortedArray.frequencyOfSortedArray(new int[]{1, 1, 1, 2, 2, 3, 3});
         frequencyOfSortedArray.frequencyOfSortedArray(new int[]{1, 1, 2, 3});
         frequencyOfSortedArray.frequencyOfSortedArray(new int[]{1});
@@ -15,12 +15,23 @@ class O14_FrequencyOfSortedArrayTest {
 
     @Test
     public void shouldPrintFrequencyOfUnsortedArrayUsingNegativeMethod() {
-        O14_FrequencyOfSortedArray frequencyOfSortedArray = new O14_FrequencyOfSortedArray();
+        O14_FrequencyOfArray frequencyOfSortedArray = new O14_FrequencyOfArray();
         int[] actual = frequencyOfSortedArray.frequencyOfUnSortedArrayUsingNegativeApproach(new int[]{1, 1, 1, 2, 2, 3, 3});
         Assertions.assertArrayEquals(new int[]{-3, -2, -2, 0, 0, 0, 0}, actual);
         actual = frequencyOfSortedArray.frequencyOfUnSortedArrayUsingNegativeApproach(new int[]{1, 1, 2, 3});
         Assertions.assertArrayEquals(new int[]{-2, -1, -1, 0}, actual);
         frequencyOfSortedArray.frequencyOfUnSortedArrayUsingNegativeApproach(new int[]{1});
+    }
+
+    @Test
+    public void shouldPrintFrequencyOfUnsortedArrayUsingAddingNMethod() {
+        O14_FrequencyOfArray frequencyOfSortedArray = new O14_FrequencyOfArray();
+        int[] actual = frequencyOfSortedArray.frequencyOfUnSortedArrayUsingAddingNApproach(new int[]{1, 1, 1, 2, 2, 3, 3});
+        Assertions.assertArrayEquals(new int[]{3, 2, 2, 0, 0, 0, 0}, actual);
+        actual = frequencyOfSortedArray.frequencyOfUnSortedArrayUsingAddingNApproach(new int[]{1, 1, 2, 3});
+        Assertions.assertArrayEquals(new int[]{2, 1, 1, 0}, actual);
+        actual = frequencyOfSortedArray.frequencyOfUnSortedArrayUsingAddingNApproach(new int[]{1});
+        Assertions.assertArrayEquals(new int[]{1}, actual);
     }
 
 }
