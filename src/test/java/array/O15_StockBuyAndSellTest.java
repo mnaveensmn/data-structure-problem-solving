be683fbeb5c287aeb534c3e6a829623f99b008fe
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-04-27 20:18:46 +0530

    array: stock buy and sell efficient solution

--- src/test/java/array/O15_StockBuyAndSellTest.java
+++ src/test/java/array/O15_StockBuyAndSellTest.java
@@ -18,4 +18,17 @@ class O15_StockBuyAndSellTest {
         Assertions.assertEquals(11, actual);
     }
 
+    @Test
+    public void shouldReturnMaxProfitUsingEfficientApproach() {
+        O15_StockBuyAndSell stockBuyAndSell = new O15_StockBuyAndSell();
+        int actual = stockBuyAndSell.maxProfitUsingEfficientSolution(new int[]{1, 5, 3, 8, 12});
+        Assertions.assertEquals(13, actual);
+        actual = stockBuyAndSell.maxProfitUsingEfficientSolution(new int[]{30, 20, 10});
+        Assertions.assertEquals(0, actual);
+        actual = stockBuyAndSell.maxProfitUsingEfficientSolution(new int[]{10, 20, 30});
+        Assertions.assertEquals(20, actual);
+        actual = stockBuyAndSell.maxProfitUsingEfficientSolution(new int[]{1, 5, 3, 1, 2, 8});
+        Assertions.assertEquals(11, actual);
+    }
+
 }
