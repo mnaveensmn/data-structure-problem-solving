717467fe88cc5ca385b6ae1c2f882eedfb990eed
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-05-02 11:20:07 +0530

    array: trapping rain water effective solution

--- src/main/java/array/O16_TrappingRainWater.java
+++ src/main/java/array/O16_TrappingRainWater.java
@@ -18,4 +18,23 @@ public class O16_TrappingRainWater {
         }
         return res;
     }
+
+    public int rainWaterCapacityUsingEffectiveSolution(int[] arr) {
+        int n = arr.length;
+        int res = 0;
+        int[] lMax = new int[arr.length];
+        int[] rMax = new int[arr.length];
+        lMax[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            lMax[i] = Math.max(arr[i], lMax[i - 1]);
+        }
+        rMax[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rMax[i] = Math.max(arr[i], rMax[i + 1]);
+        }
+        for (int i = 1; i < n - 1; i++) {
+            res += (Math.min(lMax[i], rMax[i]) - arr[i]);
+        }
+        return res;
+    }
 }
--- src/test/java/array/O16_TrappingRainWaterTest.java
+++ src/test/java/array/O16_TrappingRainWaterTest.java
@@ -6,13 +6,24 @@ import org.junit.jupiter.api.Test;
 class O16_TrappingRainWaterTest {
 
     @Test
-    public void returnCapacityOfRainWater() {
+    public void returnCapacityOfRainWaterUsingNaiveSolution() {
         O16_TrappingRainWater trappingRainWater = new O16_TrappingRainWater();
         int capacity = trappingRainWater.rainWaterCapacityUsingNaiveSolution(new int[]{3, 0, 1, 2, 5});
         Assertions.assertEquals(6, capacity);
         capacity = trappingRainWater.rainWaterCapacityUsingNaiveSolution(new int[]{2, 0, 2});
         Assertions.assertEquals(2, capacity);
         capacity = trappingRainWater.rainWaterCapacityUsingNaiveSolution(new int[]{2, 1, 1, 0, 3});
+        Assertions.assertEquals(4, capacity);
+    }
+
+    @Test
+    public void returnCapacityOfRainWaterUsingEffectiveSolution() {
+        O16_TrappingRainWater trappingRainWater = new O16_TrappingRainWater();
+        int capacity = trappingRainWater.rainWaterCapacityUsingEffectiveSolution(new int[]{3, 0, 1, 2, 5});
+        Assertions.assertEquals(6, capacity);
+        capacity = trappingRainWater.rainWaterCapacityUsingEffectiveSolution(new int[]{2, 0, 2});
+        Assertions.assertEquals(2, capacity);
+        capacity = trappingRainWater.rainWaterCapacityUsingEffectiveSolution(new int[]{2, 1, 1, 0, 3});
         Assertions.assertEquals(4, capacity);
     }
 
