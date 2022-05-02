e56bab491f6f838f4502ea362e0e29ea535a2c53
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-05-02 21:14:43 +0530

    array: max subarray sum using naive solution

---
+++ src/main/java/array/O18_MaxSubArraySum.java
@@ -0,0 +1,18 @@
+package array;
+
+public class O18_MaxSubArraySum {
+
+    public int maxSubArraySum(int[] arr) {
+        int n = arr.length;
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            int curr = 0;
+            for (int j = i; j < n; j++) {
+                curr += arr[j];
+                res = Math.max(curr, res);
+            }
+            System.out.println(i + " => " + res);
+        }
+        return res;
+    }
+}
