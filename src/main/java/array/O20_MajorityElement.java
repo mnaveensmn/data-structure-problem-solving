cdb2d12301380e5c4657835e2bf36b158a337ffd
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-05-05 14:29:39 +0530

    array: morrish voting algorithm

--- src/main/java/array/O20_MajorityElement.java
+++ src/main/java/array/O20_MajorityElement.java
@@ -17,4 +17,31 @@ public class O20_MajorityElement {
         }
         return -1;
     }
+
+    public int findMajorityUsingMorrisVotingAlgorithm(int[] arr) {
+        int n = arr.length;
+        int res = 0, count = 1;
+        for (int i = 1; i < n; i++) {
+            if (arr[res] == arr[i]) {
+                count++;
+            } else {
+                count--;
+            }
+            if (count == 0) {
+                res = i;
+                count = 1;
+            }
+        }
+        count = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[res] == arr[i]) {
+                count++;
+            }
+        }
+        if (count > (n / 2)) {
+            return res;
+        } else {
+            return -1;
+        }
+    }
 }
--- src/test/java/array/O20_MajorityElementTest.java
+++ src/test/java/array/O20_MajorityElementTest.java
@@ -16,4 +16,15 @@ class O20_MajorityElementTest {
         Assertions.assertEquals(4, actual);
     }
 
+    @Test
+    public void shouldReturnMajorityElementUsingMorrisVotingAlgorithm() {
+        O20_MajorityElement majorityElement = new O20_MajorityElement();
+        int actual = majorityElement.findMajorityUsingMorrisVotingAlgorithm(new int[]{1, 2, 1, 1});
+        Assertions.assertEquals(2, actual);
+        actual = majorityElement.findMajorityUsingMorrisVotingAlgorithm(new int[]{1, 2, 3});
+        Assertions.assertEquals(-1, actual);
+        actual = majorityElement.findMajorityUsingMorrisVotingAlgorithm(new int[]{2, 3, 4, 4, 5, 5, 5, 5, 5});
+        Assertions.assertEquals(5, actual);
+    }
+
 }
