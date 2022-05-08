f071c58a0aae01f433af91fe73b00528216fb484
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-05-08 07:21:57 +0530

    array: count distinct window

---
+++ src/main/java/array/O24_CountDistinctWindow.java
@@ -0,0 +1,28 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class O24_CountDistinctWindow {
+
+    public int[] countDistinctWindow(int[] arr, int k) {
+        ArrayList<Integer> distinctWindowCounts = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = arr.length;
+        for (int i = 0; i < k; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        distinctWindowCounts.add(map.size());
+        for (int i = k; i < n; i++) {
+            if (map.get(arr[i - k]) == 1) {
+                map.remove(arr[i - k]);
+            } else {
+                map.replace(arr[i - k], map.get(arr[i - k]) - 1);
+            }
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            distinctWindowCounts.add(map.size());
+        }
+        return distinctWindowCounts.stream().mapToInt(i -> i).toArray();
+    }
+}
---
+++ src/test/java/array/O24_CountDistinctWindowTest.java
@@ -0,0 +1,17 @@
+package array;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class O24_CountDistinctWindowTest {
+
+    @Test
+    public void shouldReturnDistinctCountOfWindow() {
+        O24_CountDistinctWindow countDistinctWindow = new O24_CountDistinctWindow();
+        int[] actual = countDistinctWindow.countDistinctWindow(new int[]{1, 2, 1, 3, 4, 2, 3}, 4);
+        Assertions.assertArrayEquals(new int[]{3, 4, 4, 3}, actual);
+        actual = countDistinctWindow.countDistinctWindow(new int[]{1,1,3,3,2,4,2,4,5,6}, 3);
+        Assertions.assertArrayEquals(new int[]{3, 4, 4, 3}, actual);
+    }
+
+}
