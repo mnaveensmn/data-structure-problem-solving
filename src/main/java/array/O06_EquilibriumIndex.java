5c8408fb7fa91e51f96aa83aaea5052847a07be8
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-05-10 14:29:33 +0530

    array: equilibrium index

--- src/main/java/array/O06_EquilibriumIndex.java
+++ src/main/java/array/O06_EquilibriumIndex.java
@@ -2,12 +2,14 @@ package array;
 
 public class O06_EquilibriumIndex {
 
+    //redid - 1
     public int eqIndex(int[] arr) {
+        int n = arr.length;
         int sum = 0, leftSum = 0;
-        for (int i : arr) {
-            sum += i;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
         }
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < n; i++) {
             sum -= arr[i];
             if (leftSum == sum) {
                 return i;
