bf307f02048507a867c8a155f73a3b340779fe1f
mnaveensmn@gmail.com <mnaveensmn@gmail.com>
2022-05-13 22:26:30 +0530

    bit: Added the comment

--- src/main/java/bitwise/O01_BitTactics.java
+++ src/main/java/bitwise/O01_BitTactics.java
@@ -10,7 +10,7 @@ public class O01_BitTactics {
         //Step 2: Perform OR operation on leftShifted and n
         return n | leftShifted;
     }
-
+// << 
     public int unSetBit(int n, int position) {
         //Step 1: Left shift 1 to given position
         int leftShifted = 1 << position;
