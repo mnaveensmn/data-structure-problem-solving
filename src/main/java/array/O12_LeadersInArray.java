8937a20c10647038ff4ed6190411adca15411d7f
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-05-15 23:48:48 +0530

    array: leaders in the array

--- src/main/java/array/O12_LeadersInArray.java
+++ src/main/java/array/O12_LeadersInArray.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 
 public class O12_LeadersInArray {
 
-    //redid - 1
+    //redid - 2
     public int[] leadersInArray(int[] arr) {
         int n = arr.length;
         int currLeader = arr[n - 1];
         ArrayList<Integer> leaders = new ArrayList<>();
         leaders.add(currLeader);
         for (int i = n - 2; i >= 0; i--) {
-            if (currLeader < arr[i]) {
+            if (arr[i] > currLeader) {
                 currLeader = arr[i];
                 leaders.add(arr[i]);
             }
