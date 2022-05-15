cae94ec867095ee61642cd8c946f9a32371fd4ad
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-05-15 23:58:34 +0530

    array: last non zero index

--- src/main/java/array/O11_MoveZerosEnd.java
+++ src/main/java/array/O11_MoveZerosEnd.java
@@ -2,11 +2,11 @@ package array;
 
 public class O11_MoveZerosEnd {
 
-    //redid - 1
+    //redid - 2
     public void moveZeros(int[] arr) {
         int n = arr.length;
         int lastNonZeroIndex = 0;
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < n; i++) {
             if (arr[i] != 0) {
                 int temp = arr[lastNonZeroIndex];
                 arr[lastNonZeroIndex] = arr[i];
