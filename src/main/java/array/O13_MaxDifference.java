39717a682841bf92dee7bd77b195505ffc06251d
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-05-16 23:55:35 +0530

    array: max difference

--- src/main/java/array/O13_MaxDifference.java
+++ src/main/java/array/O13_MaxDifference.java
@@ -2,7 +2,7 @@ package array;
 
 public class O13_MaxDifference {
 
-    //redid - 1
+    //redid - 2
     public int findMaxDifference(int[] arr) {
         int n = arr.length;
         if (n <= 1) {
@@ -10,9 +10,9 @@ public class O13_MaxDifference {
         }
         int res = arr[1] - arr[0];
         int minValue = arr[0];
-        for (int j = 1; j < n; j++) {
-            res = Math.max(res, arr[j] - minValue);
-            minValue = Math.min(minValue, arr[j]);
+        for (int i = 1; i < n; i++) {
+            res = Math.max(res, arr[i] - minValue);
+            minValue = Math.min(minValue, arr[i]);
         }
         return res;
     }
