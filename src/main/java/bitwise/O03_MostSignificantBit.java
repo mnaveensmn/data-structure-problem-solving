9ae8da7df02ae58e323a09cf1ef087e3f96ee4a9
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-05-16 23:58:27 +0530

    bitwise: msb

--- src/main/java/bitwise/O03_MostSignificantBit.java
+++ src/main/java/bitwise/O03_MostSignificantBit.java
@@ -1,7 +1,7 @@
 package bitwise;
 
 public class O03_MostSignificantBit {
-    //Redid - 3
+    //Redid - 4
     public int msb(int n) {
         int msb = (int) Math.floor(Math.log(n) / Math.log(2));
         return (int) Math.pow(2, msb);
