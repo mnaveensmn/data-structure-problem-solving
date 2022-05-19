605560044ac824fcbeffe4f64196b011b308a297
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-05-19 23:46:27 +0530

    array: count distinct window

--- src/main/java/array/O24_CountDistinctWindow.java
+++ src/main/java/array/O24_CountDistinctWindow.java
@@ -8,22 +8,21 @@ public class O24_CountDistinctWindow {
 
     //redid - 1
     public int[] countDistinctWindow(int[] arr, int k) {
-        ArrayList<Integer> distinctWindowCounts = new ArrayList<>();
+        ArrayList<Integer> counts = new ArrayList<>();
         Map<Integer, Integer> map = new HashMap<>();
-        int n = arr.length;
         for (int i = 0; i < k; i++) {
             map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
-        distinctWindowCounts.add(map.size());
-        for (int i = k; i < n; i++) {
+        counts.add(map.size());
+        for (int i = k; i < arr.length; i++) {
             if (map.get(arr[i - k]) == 1) {
                 map.remove(arr[i - k]);
             } else {
-                map.replace(arr[i - k], map.get(arr[i - k]) - 1);
+                map.put(arr[i - k], map.get(arr[i - k]) - 1);
             }
             map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
-            distinctWindowCounts.add(map.size());
+            counts.add(map.size());
         }
-        return distinctWindowCounts.stream().mapToInt(i -> i).toArray();
+        return counts.stream().mapToInt(i -> i).toArray();
     }
 }
--- src/test/java/array/O24_CountDistinctWindowTest.java
+++ src/test/java/array/O24_CountDistinctWindowTest.java
@@ -10,8 +10,8 @@ class O24_CountDistinctWindowTest {
         O24_CountDistinctWindow countDistinctWindow = new O24_CountDistinctWindow();
         int[] actual = countDistinctWindow.countDistinctWindow(new int[]{1, 2, 1, 3, 4, 2, 3}, 4);
         Assertions.assertArrayEquals(new int[]{3, 4, 4, 3}, actual);
-        actual = countDistinctWindow.countDistinctWindow(new int[]{1,1,3,3,2,4,2,4,5,6}, 3);
-        Assertions.assertArrayEquals(new int[]{3, 4, 4, 3}, actual);
+        actual = countDistinctWindow.countDistinctWindow(new int[]{1, 1, 3, 3, 2, 4, 2, 4, 5, 6}, 3);
+        Assertions.assertArrayEquals(new int[]{2, 2, 2, 3, 2, 2, 3, 3}, actual);
     }
 
 }
