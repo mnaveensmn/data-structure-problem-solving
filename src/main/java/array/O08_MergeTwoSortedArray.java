6787b8e90036110cc1a3fde3f513fccf666d5263
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-05-21 23:58:04 +0530

    array: merge two sorted array

--- src/main/java/array/O08_MergeTwoSortedArray.java
+++ src/main/java/array/O08_MergeTwoSortedArray.java
@@ -2,27 +2,25 @@ package array;
 
 public class O08_MergeTwoSortedArray {
 
+    //redid - 1
     public int[] merge(int[] arr1, int[] arr2) {
-        int m = arr1.length;
-        int n = arr2.length;
-        int[] arr3 = new int[m + n];
+        int n = arr1.length;
+        int m = arr2.length;
         int i = 0, j = 0, k = 0;
-
-        while (i < m && j < n) {
+        int[] arr3 = new int[n + m];
+        while (i < n && j < m) {
             if (arr1[i] < arr2[j]) {
                 arr3[k++] = arr1[i++];
             } else {
                 arr3[k++] = arr2[j++];
             }
         }
-
-        while (i < m) {
+        while (i < n) {
             arr3[k++] = arr1[i++];
         }
         while (j < n) {
             arr3[k++] = arr2[j++];
         }
-
         return arr3;
     }
 }
