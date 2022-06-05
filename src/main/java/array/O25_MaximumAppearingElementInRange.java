18c7ef1edf8b74e21a19c5d0592cd5fa69c028ce
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-06-05 23:06:30 +0530

    array: maximum appearing element in range

--- src/main/java/array/O25_MaximumAppearingElementInRange.java
+++ src/main/java/array/O25_MaximumAppearingElementInRange.java
@@ -2,18 +2,18 @@ package array;
 
 public class O25_MaximumAppearingElementInRange {
 
-    //redid - 2
+    //redid - 3
     public int maximumAppearingElementInGivenRange(int[] l, int[] r) {
         int[] arr = new int[1000];
+        int res = 0, n = arr.length;
         for (int i = 0; i < l.length; i++) {
             arr[l[i]]++;
-            if (r[i] < 999) {
+            if (r[i] + 1 < n) {
                 arr[r[i] + 1]--;
             }
         }
         int maxCount = arr[0];
-        int res = 0;
-        for (int i = 1; i < arr.length; i++) {
+        for (int i = 1; i < n; i++) {
             arr[i] = arr[i] + arr[i - 1];
             if (maxCount < arr[i]) {
                 maxCount = arr[i];
