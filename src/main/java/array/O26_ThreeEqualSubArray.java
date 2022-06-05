02c65c62dbd177559b3cf774bb693b58de6252d7
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-06-05 22:56:21 +0530

    array: three equal sub array

--- src/main/java/array/O26_ThreeEqualSubArray.java
+++ src/main/java/array/O26_ThreeEqualSubArray.java
@@ -2,21 +2,24 @@ package array;
 
 public class O26_ThreeEqualSubArray {
 
-    //redid - 2
+    //redid - 3
     public String findSplit(int[] arr) {
-        int preSum = 0;
-        int i1 = -1, i2 = -1;
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
+        int preSum = 0,
+                sum = 0,
+                n = arr.length,
+                i1 = -1,
+                i2 = -1, s1, s2;
+
+        for (int i = 0; i < n; i++) {
             sum += arr[i];
         }
-        int S1 = sum / 3;
-        int S2 = 2 * S1;
-        for (int i = 0; i < arr.length; i++) {
+        s1 = sum / 3;
+        s2 = 2 * s1;
+        for (int i = 0; i < n; i++) {
             preSum += arr[i];
-            if (preSum == S1 && i1 == -1) {
+            if (preSum == s1 && i1 == -1) {
                 i1 = i;
-            } else if (preSum == S2 && i1 != -1) {
+            } else if (preSum == s2 && i1 != -1) {
                 i2 = i;
             }
         }
